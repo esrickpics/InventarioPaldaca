0ba32d424a50d06d52bb6d727fbdabd7c170ee6d
Ricardo Goitia <ricardogoitia108@gmail.com>
2025-03-27 15:12:15 -0400

    ignore

--- InventarioPaldaca/obj/Debug/net8.0/InventarioPaldaca.AssemblyInfo.cs
+++ InventarioPaldaca/obj/Debug/net8.0/InventarioPaldaca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventarioPaldaca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be677aaa347260b6fbd3f565737604d961e4af0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dd681205e5d775d0dbd41b61a52d9e506367af6")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventarioPaldaca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventarioPaldaca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
